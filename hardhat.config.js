6f9d625aac135b46e2a3b0ed508a7997bcde1f17
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-03-18 09:57:05 +0530

    upgradable-contracts

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
